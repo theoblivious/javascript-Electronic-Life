23549bcacdfe535d9c0f1006aa306cbe10f7f9aa
theoblivious <hashimoto.kenn@gmail.com>
2014-11-09 16:05:12 -0500

    making headway in developing the world.

---
+++ electronicLife.js
@@ -0,0 +1,133 @@
+// creating a grid of what our world would look like. 'o' represents the person in our world.
+// the '#' represents rocks and impassible objects.
+var plan = ["############################",
+    "#      #    #      o      ##",
+    "#                          #",
+    "#          #####           #",
+    "##         #   #    ##     #",
+    "###           ##     #     #",
+    "#           ###      #     #",
+    "#   ####                   #",
+    "#   ##       o             #",
+    "# o  #         o       ### #",
+    "#    #                     #",
+    "############################"
+];
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
+
+//This code defines the Grid object, with some basic methods:
+function Grid(width, height) {
+    this.space = new Array(width * height);
+    this.width = width;
+    this.height = height;
+}
+Grid.prototype.isInside = function(vector) {
+    return vector.x >= 0 && vector.x < this.width &&
+        vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector) {
+    return this.space[vector.x + this.width * vector.y];
+};
+Grid.prototype.set = function(vector, value) {
+    this.space[vector.x + this.width * vector.y] = value;
+};
+
+//giving directions by using the vector object
+var directions = {
+    "n": new Vector(0, -1),
+    "ne": new Vector(1, -1),
+    "e": new Vector(1, 0),
+    "se": new Vector(1, 1),
+    "s": new Vector(0, 1),
+    "sw": new Vector(-1, 1),
+    "w": new Vector(-1, 0),
+    "nw": new Vector(-1, -1)
+};
+
+// simple stupid critter that  bounces around until it hits an obstacle and bounces into a random direction
+//random element array picks a random elemnt from an array. the critter picks a random direction by calling this.
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+function BouncingCritter() {
+    this.direction = randomElement(directionNames);
+}
+
+BouncingCritter.prototype.act = function(view) {
+    if (view.look(this.direction) != " ")
+        this.direction = view.find(" ") || "s";
+    return {
+        type: "move",
+        direction: this.direction
+    };
+};
+
+//world object
+
+function elementFromChar(legend, ch) {
+    if (ch == " ")
+        return null;
+    var element = new legend[ch]();
+    element.originChar = ch;
+    return element;
+}
+
+function World(map, legend) {
+    var grid = new Grid(map[0].length, map.length);
+    this.grid = grid;
+    this.legend = legend;
+
+    map.forEach(function(line, y) {
+        for (var x = 0; x < line.length; x++)
+            grid.set(new Vector(x, y),
+                elementFromChar(legend, line[x]));
+    });
+}
+
+function charFromElement(element) {
+    if (element == null)
+        return " ";
+    else
+        return element.originChar;
+}
+
+World.prototype.toString = function() {
+    var output = "";
+    for (var y = 0; y < this.grid.height; y++) {
+        for (var x = 0; x < this.grid.width; x++) {
+            var element = this.grid.get(new Vector(x, y));
+            output += charFromElement(element);
+        }
+        output += "\n";
+    }
+    return output;
+};
+
+var world = new World(plan, {"#": Wall,
+                             "o": BouncingCritter});
+console.log(world.toString());
+// → ############################
+//   #      #    #      o      ##
+//   #                          #
+//   #          #####           #
+//   ##         #   #    ##     #
+//   ###           ##     #     #
+//   #           ###      #     #
+//   #   ####                   #
+//   #   ##       o             #
+//   # o  #         o       ### #
+//   #    #                     #
+//   ############################
+
+
